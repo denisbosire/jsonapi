bcef32f3c2ba9bf401754e48c8c7cf332c5c05c5
denisbosire <dbosire88@gmail.com>
2021-01-03 19:19:00 +0300

    Test
    Fixed issue with ABSPATH
    created tests

--- jsonapi.php
+++ jsonapi.php
@@ -79,7 +79,7 @@ register_deactivation_hook( __FILE__, 'deactivate_jsonapi' );
  * admin-specific hooks, and public-facing site hooks.
  */
 require plugin_dir_path( __FILE__ ) . 'includes/class-jsonapi.php';
-require_once plugin_dir_url( __FILE__ ) . 'vendor/autoload.php';
+//require_once plugin_dir_url( __FILE__ ) . 'vendor/autoload.php';
 
 
 /**
