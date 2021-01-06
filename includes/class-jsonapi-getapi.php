2da27a0859ca56958345b8b105ab0cd255e79646
denisbosire <dbosire88@gmail.com>
2021-01-06 21:09:38 +0300

    Phpcs in Phpstorm

--- includes/class-jsonapi-getapi.php
+++ includes/class-jsonapi-getapi.php
@@ -1,9 +1,7 @@
 <?php
-//namespace Jsonapi;
 
-// require_once plugin_dir_url( __DIR__ ) . 'vendor/autoload.php';
-require dirname( __FILE__ ) . '/../vendor/autoload.php';
-// loca the class
+require dirname(__FILE__) . '/../vendor/autoload.php';
+
 use GuzzleHttp\Client;
 
 /**
@@ -26,309 +24,319 @@ use GuzzleHttp\Client;
  * @subpackage Jsonapi/admin
  * @author     Denis Bosire <denischweya@gmail.com>
  */
-class Get_Rest_Api {
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
-	private $plugin_name;
+class Get_Rest_Api
+{
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $plugin_name The ID of this plugin.
+     */
+    private $plugin_name;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string $version The current version of this plugin.
+     */
+    private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string $plugin_name       The name of this plugin.
-	 * @param      string $version    The version of this plugin.
-	 */
-	public function __construct( $plugin_name, $version ) {
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @param string $plugin_name The name of this plugin.
+     * @param string $version The version of this plugin.
+     * @since    1.0.0
+     */
+    public function __construct($plugin_name, $version)
+    {
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+    }
 
-		$this->plugin_name = $plugin_name;
-		$this->version     = $version;
-
-	}
-	/**
+    /**
      * Load CSS.
      * */
-	public function enqueue_styles(){
+    public function enqueue_styles()
+    {
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'public/css/jsonapi-public.css', array(), $this->version, 'all');
+    }
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'public/css/jsonapi-public.css', array(), $this->version, 'all' );
+    /**
+     * Register the JavaScript for the admin area, also localizes the scripts
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts()
+    {
+        wp_enqueue_script('jsonapi', plugin_dir_url(__DIR__) . 'public/js/jsonapi-public.js', array('jquery'), $this->version, false);
+        wp_localize_script(
+            'jsonapi',
+            'jsonapi',
+            array(
+                'ajaxurl' => admin_url('admin-ajax.php'),
+                'nonce' => wp_create_nonce('ajax-nonce'),
+            )
+        );
+    }
 
-	}
-
-	/**
-	 * Register the JavaScript for the admin area, also localizes the scripts
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		wp_enqueue_script( 'jsonapi', plugin_dir_url( __DIR__ ) . 'public/js/jsonapi-public.js', array( 'jquery' ), $this->version, false );
-		wp_localize_script(
-			'jsonapi',
-			'jsonapi',
-			array(
-				'ajaxurl' => admin_url( 'admin-ajax.php' ),
-				'nonce'   => wp_create_nonce( 'ajax-nonce' ),
-			)
-		);
-
-	}
-
-	// Print the markup for the page
-	public function jsonapi_page(){
-		// this is the initial table for users
-		echo '<h2 class="page-title">'. esc_html__( 'Rest Api Placeholder', 'jsonapi' ) .'</h2>';
-		echo '<div id="loader" class="lds-dual-ring hidden overlay"></div>';
-		// Could use as well, wp_remote_get
-		$client = new Client(
-			array(
-				// Base URI is used with relative requests
-				'base_uri' => 'https://jsonplaceholder.typicode.com/',
-			)
-		);
-		// use guzzle to fetch data, could also use wp_remote_request()
-		$response = $client->request(
-			'GET',
-			'/users',
-			array(
-				'query' => array(
-					'page' => '2',
-				),
-			)
-		);
+    // Print the markup for the page
+    public function jsonapi_page()
+    {
+        // this is the initial table for users
+        echo '<h2 class="page-title">' . esc_html__('Rest Api Placeholder', 'jsonapi') . '</h2>';
+        echo '<div id="loader" class="lds-dual-ring hidden overlay"></div>';
+        // Could use as well, wp_remote_get
+        $client = new Client(
+            array(
+                // Base URI is used with relative requests
+                'base_uri' => 'https://jsonplaceholder.typicode.com/',
+            )
+        );
+        // use guzzle to fetch data, could also use wp_remote_request()
+        $response = $client->request(
+            'GET',
+            '/users',
+            array(
+                'query' => array(
+                    'page' => '2',
+                ),
+            )
+        );
 
 
-		$body     = $response->getBody();
-		$arr_body = json_decode( $body, true );
-		// start creating the layout.
-		?>
-		<div id="usercontent">
-			<a href="#" class="bckbtn"><span class="dashicons dashicons-arrow-left-alt"><?php esc_html_e( 'Back', 'jsonapi' ); ?></span></a>
+        $body = $response->getBody();
+        $arr_body = json_decode($body, true); ?>
+        <div id="usercontent">
+            <a href="#" class="bckbtn"><span
+                        class="dashicons dashicons-arrow-left-alt"><?php esc_html_e('Back', 'jsonapi'); ?></span></a>
 
-			<div class="ajax-data">
-				<div class="postlist"></div>
-				<div class="todos"></div>
-				<div class="albums">
-			</div> 
-			</div>
-		</div>
-		<div id="content-area">
+            <div class="ajax-data">
+                <div class="postlist"></div>
+                <div class="todos"></div>
+                <div class="albums">
+                </div>
+            </div>
+        </div>
+        <div id="content-area">
 
-		<table class="data-table">
-			<thead>
-				<tr style="text-align:left;">
-					<th><?php esc_html_e( 'User ID:', 'jsonapi' ); ?></th> 
-					<th><?php esc_html_e( 'Name:', 'jsonapi' ); ?></th> 
-					<th><?php esc_html_e( 'Email:', 'jsonapi' ); ?></th> 
-					<th><?php esc_html_e( 'Username:', 'jsonapi' ); ?></th> 
-					<th><?php esc_html_e( 'City:', 'jsonapi' ); ?></th> 
-					<th><?php esc_html_e( 'Phone:', 'jsonapi' ); ?></th> 
-					<th><?php esc_html_e( 'Website:', 'jsonapi' ); ?></th> 
-					<th><?php esc_html_e( 'Company:', 'jsonapi' ); ?></th>
+            <table class="data-table">
+                <thead>
+                <tr style="text-align:left;">
+                    <th><?php esc_html_e('User ID:', 'jsonapi'); ?></th>
+                    <th><?php esc_html_e('Name:', 'jsonapi'); ?></th>
+                    <th><?php esc_html_e('Email:', 'jsonapi'); ?></th>
+                    <th><?php esc_html_e('Username:', 'jsonapi'); ?></th>
+                    <th><?php esc_html_e('City:', 'jsonapi'); ?></th>
+                    <th><?php esc_html_e('Phone:', 'jsonapi'); ?></th>
+                    <th><?php esc_html_e('Website:', 'jsonapi'); ?></th>
+                    <th><?php esc_html_e('Company:', 'jsonapi'); ?></th>
 
-				</tr>
-			</thead>
-				<?php foreach ( $arr_body as $value ) { ?>
-					<tr>
-						<td><a href="#" data-userid="<?php echo $value['id']; ?>" class="userid"><?php echo $value['id']; ?> </a></td> 
-						<td><a href="#" data-userid="<?php echo $value['id']; ?>" class="userid"><?php echo $value['name']; ?></a></td> 
-						<td><a href="#" data-userid="<?php echo $value['id']; ?>" class="userid"><?php echo $value['email']; ?></a></td>
-						<td><a href="#" data-userid="<?php echo $value['id']; ?>" class="userid"><?php echo $value['username']; ?></a></td>
-						<td><?php echo $value['address']['city']; ?> </td>
-						<td><?php echo $value['phone']; ?> </td>
-						<td><?php echo $value['website']; ?> </td>
-						<td><?php echo $value['company']['name']; ?> </td>
-					</tr>
-				<?php } ?>
-		</table>    
-		</div>   
-		<?php
+                </tr>
+                </thead>
+                <?php foreach ($arr_body as $value) { ?>
+                    <tr>
+                        <td><a href="#" data-userid="<?php echo $value['id']; ?>"
+                               class="userid"><?php echo $value['id']; ?> </a></td>
+                        <td><a href="#" data-userid="<?php echo $value['id']; ?>"
+                               class="userid"><?php echo $value['name']; ?></a></td>
+                        <td><a href="#" data-userid="<?php echo $value['id']; ?>"
+                               class="userid"><?php echo $value['email']; ?></a></td>
+                        <td><a href="#" data-userid="<?php echo $value['id']; ?>"
+                               class="userid"><?php echo $value['username']; ?></a></td>
+                        <td><?php echo $value['address']['city']; ?> </td>
+                        <td><?php echo $value['phone']; ?> </td>
+                        <td><?php echo $value['website']; ?> </td>
+                        <td><?php echo $value['company']['name']; ?> </td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+        <?php
+    }
+    /**
+     * Get Posts
+     */
+    public function get_user_posts()
+    {
+        // refactor the code below, it repeats several times
+        $client = new Client(
+            array(
+                // Base URI is used with relative requests
+                'base_uri' => 'https://jsonplaceholder.typicode.com/',
+            )
+        );
 
-	}
+        $response = $client->request(
+            'GET',
+            '/posts',
+            array(
+                'query' => array(
+                    'page' => '1',
+                ),
+            )
+        );
 
-	public function get_user_posts() {
-		// refactor the code below, it repeats several times
-		$client = new Client(
-			array(
-				// Base URI is used with relative requests
-				'base_uri' => 'https://jsonplaceholder.typicode.com/',
-			)
-		);
+        $body = $response->getBody();
+        $arr_body = json_decode($body, true);
+        // set userID
+        $userID = $_GET['userid'];
+        $userPosts = array();
+        $postID = array();
 
-		$response = $client->request(
-			'GET',
-			'/posts',
-			array(
-				'query' => array(
-					'page' => '1',
-				),
-			)
-		);
+        foreach ($arr_body as $key => $value) {
+            if ($userID == $value['userId']) {
+                $userPosts[] = $value;
+                $postID[] = $value['id'];
+            }
+        }
+        // sends the data to be ajax'd
+        wp_send_json_success($userPosts);
+    }
+    /**
+     * Get TODOs
+     */
+    public function get_user_todos()
+    {
+        // refactor the code below, it repeats several times
+        $client = new Client(
+            array(
+                // Base URI is used with relative requests
+                'base_uri' => 'https://jsonplaceholder.typicode.com',
+            )
+        );
 
-		$body     = $response->getBody();
-		$arr_body = json_decode( $body, true );
-		// set userID
-		$userID    = $_GET['userid'];
-		$userPosts = array();
-		$postID    = array();
+        $response = $client->request(
+            'GET',
+            '/todos',
+            array(
+                'query' => array(
+                    'page' => '1',
+                ),
+            )
+        );
 
-		foreach ( $arr_body as $key => $value ) {
-			if ( $userID == $value['userId'] ) {
-				$userPosts[] = $value;
-				$postID[]    = $value['id'];
-			}
-		}
-		// sends the data to be ajax'd
-		wp_send_json_success( $userPosts );
+        $body = $response->getBody();
+        $arr_body = json_decode($body, true);
+        $userID = $_GET['userid'];
+        $userTodo = array();
 
-	}
+        foreach ($arr_body as $key => $value) {
+            if ($userID == $value['userId']) {
+                $userTodo[] = $value;
+            }
+        }
+        wp_send_json_success($userTodo);
+    }
 
-	public function get_user_todos() {
-		// refactor the code below, it repeats several times
-		$client = new Client(
-			array(
-				// Base URI is used with relative requests
-				'base_uri' => 'https://jsonplaceholder.typicode.com',
-			)
-		);
+    /**
+     * Get Albums
+     */
+    public function get_user_albums()
+    {
+        // refactor the code below, it repeats several times
+        $client = new Client(
+            array(
+                // Base URI is used with relative requests
+                'base_uri' => 'https://jsonplaceholder.typicode.com',
+            )
+        );
 
-		$response = $client->request(
-			'GET',
-			'/todos',
-			array(
-				'query' => array(
-					'page' => '1',
-				),
-			)
-		);
+        $response = $client->request('GET', '/albums');
 
-		$body     = $response->getBody();
-		$arr_body = json_decode( $body, true );
-		$userID   = $_GET['userid'];
-		$userTodo = array();
+        $body = $response->getBody();
+        $arr_body = json_decode($body, true);
+        $userID = $_GET['userid'];
+        $albums = array();
+        $albumID = array();
 
-		foreach ( $arr_body as $key => $value ) {
-			if ( $userID == $value['userId'] ) {
-				$userTodo[] = $value;
-			}
-		}
-		// var_export($userPosts);
-		wp_send_json_success( $userTodo );
+        foreach ($arr_body as $key => $value) {
+            if ($userID == $value['userId']) {
+                $albums[] = $value;
+                $album['id'] = $albumID;
+            }
+        }
 
-	}
-	public function get_user_albums() {
-		// refactor the code below, it repeats several times
-		$client = new Client(
-			array(
-				// Base URI is used with relative requests
-				'base_uri' => 'https://jsonplaceholder.typicode.com',
-			)
-		);
+        wp_send_json_success($albums);
+    }
 
-		$response = $client->request( 'GET', '/albums' );
+    public function get_photos()
+    {
+        $client = new Client(
+            array(
+                // Base URI is used with relative requests
+                'base_uri' => 'https://jsonplaceholder.typicode.com',
+            )
+        );
 
-		$body     = $response->getBody();
-		$arr_body = json_decode( $body, true );
-		$userID   = $_GET['userid'];
-		$albums   = array();
-		$albumID  = array();
+        $response = $client->request(
+            'GET',
+            '/photos',
+            array(
+                'query' => array(
+                    'page' => '1',
+                ),
+            )
+        );
 
-		foreach ( $arr_body as $key => $value ) {
-			if ( $userID == $value['userId'] ) {
-				$albums[]    = $value;
-				$album['id'] = $albumID;
-			}
-		}
+        $body = $response->getBody();
+        $arr_body = json_decode($body, true);
+        // set albumID, to be used for photos
+        $albumID = $_GET['albumid'];
+        $photos = array();
 
-		wp_send_json_success( $albums );
+        foreach ($arr_body as $key => $value) {
+            if ($albumID == $value['albumId']) {
+                $photos[] = $value;
+                // $album['id'] = $albumID
+            }
+        }
+        wp_send_json_success($photos);
+    }
 
-	}
+    public function get_comments()
+    {
+        $client = new Client(
+            array(
+                // Base URI is used with relative requests
+                'base_uri' => 'https://jsonplaceholder.typicode.com',
+            )
+        );
 
-	public function get_photos() {
-		$client = new Client(
-			array(
-				// Base URI is used with relative requests
-				'base_uri' => 'https://jsonplaceholder.typicode.com',
-			)
-		);
+        $response = $client->request(
+            'GET',
+            '/comments',
+            array(
+                'query' => array(
+                    'page' => '1',
+                ),
+            )
+        );
 
-		$response = $client->request(
-			'GET',
-			'/photos',
-			array(
-				'query' => array(
-					'page' => '1',
-				),
-			)
-		);
+        $body = $response->getBody();
+        $arr_body = json_decode($body, true);
+        $postID = $_GET['postid'];
+        $comments = array();
 
-		$body     = $response->getBody();
-		$arr_body = json_decode( $body, true );
-		// set albumID, to be used for photos
-		$albumID = $_GET['albumid'];
-		$photos  = array();
+        foreach ($arr_body as $key => $value) {
+            if ($postID == $value['postId']) {
+                $comments[] = $value;
+                // $album['id'] = $albumID
+            }
+        }
+        wp_send_json_success($comments);
+    }
+    /**
+     * set the custom_url, *must be a better way to do this.
+     */
 
-		foreach ( $arr_body as $key => $value ) {
-			if ( $albumID == $value['albumId'] ) {
-				$photos[] = $value;
-				// $album['id'] = $albumID
-			}
-		}
-		wp_send_json_success( $photos );
-
-	}
-	public function get_comments() {
-		$client = new Client(
-			array(
-				// Base URI is used with relative requests
-				'base_uri' => 'https://jsonplaceholder.typicode.com',
-			)
-		);
-
-		$response = $client->request(
-			'GET',
-			'/comments',
-			array(
-				'query' => array(
-					'page' => '1',
-				),
-			)
-		);
-
-		$body     = $response->getBody();
-		$arr_body = json_decode( $body, true );
-		$postID   = $_GET['postid'];
-		$comments = array();
-
-		foreach ( $arr_body as $key => $value ) {
-			if ( $postID == $value['postId'] ) {
-				$comments[] = $value;
-				// $album['id'] = $albumID
-			}
-		}
-		wp_send_json_success( $comments );
-
-	}
-	// set the custom_url, *must be a better way to do this
-	public function my_custom_url_handler() {
-		// wp
-		if ( $_SERVER['REQUEST_URI'] == '/custom_url' ) {
-			// $this->jsonapi_page();
-			$template = plugin_dir_url( __DIR__ ) . 'templates/custom-url-template.php';
-			// exit();
-		}
-	}
+    public function my_custom_url_handler()
+    {
+        if ($_SERVER['REQUEST_URI'] == '/custom_url') {
+            $template = plugin_dir_url(__DIR__) . 'templates/custom-url-template.php';
+        }
+    }
 }
-// $getapi = new GetApi();
--- includes/class-jsonapi-template.php
+++ includes/class-jsonapi-template.php
@@ -1,32 +1,46 @@
 <?php
-class Template_Loader {
+
+/**
+ * Class Template_Loader
+ * Loads the custom templates for custom_url.
+ */
+class Template_Loader
+{
 
 
+    /**
+     * Re Write rules, currently not in use.
+     */
+    public function jsonapi_rewite()
+    {
+        add_rewrite_rule('custom_url/(\d*)$', 'index.php?custom_url', 'top');
+    }
 
-	public function jsonapi_rewite() {
-		add_rewrite_rule( 'custom_url/(\d*)$', 'index.php?custom_url', 'top' );
-	}
+    public function jsonapi_custom_query_vars($query_vars)
+    {
+        $query_vars[] = 'custom_url';
+        return $query_vars;
+        var_dump($query_vars);
+    }
+    /**
+     * check if custom_url is available, if so, load a custom template file.
+     */
+    public function jsonapi_custom_template($template)
+    {
+        if ($_SERVER['REQUEST_URI'] == '/custom_url') {
+            // load the file if exists
+            $template = plugin_dir_url(__DIR__) . 'templates/custom-url-template.php';
+        } else {
+            // die();
+        }
 
-	public function jsonapi_custom_query_vars( $query_vars ) {
-		$query_vars[] = 'custom_url';
-		return $query_vars;
-		var_dump( $query_vars );
-	}
-	// check if custom_url is available, if so, load a custom template file.
-	public function jsonapi_custom_template( $template ) {
-		if ( $_SERVER['REQUEST_URI'] == '/custom_url' ) {
-			// load the file if exists
-			$template = plugin_dir_url( __DIR__ ) . 'templates/custom-url-template.php';
-
-		} else {
-			// die();
-		}
-
-		return $template;
-
-	}
-	// Load dashicons
-	public function jsonapi_load_dashicons() {
-		wp_enqueue_style( 'dashicons' );
-	}
+        return $template;
+    }
+    /**
+     * Load Dashicons
+     */
+    public function jsonapi_load_dashicons()
+    {
+        wp_enqueue_style('dashicons');
+    }
 }
